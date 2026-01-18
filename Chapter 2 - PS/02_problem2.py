249575ae13496598cda6f70446e4f1cf8666ed86
rutujasomwanshi09 <rutujasomwanshi09@gmail.com>
2026-01-18 22:21:00 +0530

    Day 1

---
+++ Chapter 2 - PS/02_problem2.py
@@ -0,0 +1,5 @@
+a = 37
+
+b = 5
+
+print("Reminder when a is devided by b is", a % b)
---
+++ Chapter 2/03_rules_variables.py
@@ -0,0 +1,12 @@
+a = 23
+
+aaa = 453
+
+Rutuja = 34
+
+sameer = 56
+
+_sameer = 65
+
+#  @sameer = 45  # Invalid due to @ symbol
+# s@meer  # Invalid due to @ symbol

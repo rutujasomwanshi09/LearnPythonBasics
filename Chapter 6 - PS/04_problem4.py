c74d6a16312e977a4bdb022d2fb9003bef80313b
rutujasomwanshi09 <rutujasomwanshi09@gmail.com>
2026-01-20 22:48:07 +0530

    Day 3

---
+++ Chapter 6 - PS/04_problem4.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+
+if(len(username)<10):
+    print("Your username contains less than 10 characters")
+
+else:
+    print("All is well!")    
---
+++ Chapter 6 - PS/07_problem7.py
@@ -0,0 +1,7 @@
+post = input("Enter the post: ")
+
+if("Harry" in post.lower()):
+    print("This post is talking about harry")
+
+else:
+    print("This post is not talking about harry")    
